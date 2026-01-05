5481dd559a9b63040bbff7902e310d3ebc1a3683
Ashish Raj <ashishraj43170@gmail.com>
2026-01-05 20:40:00 +0530

    index_setup

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express')
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
+
+app.use(express.static('/public'));
+
+server.listen(5000, ()=> console.log('server listening at port: 5000'));
